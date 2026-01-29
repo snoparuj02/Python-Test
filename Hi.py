b6dea95d20ec9965166dc4f76332efae2b226010
Tor <s.noparuj.02@gmail.com>
2026-01-29 04:02:08 +0000

    add new function

--- Hi.py
+++ Hi.py
@@ -1,3 +1,10 @@
 ## for posit test
 print("Hi")
 print("I am wrtiting on github with posit.cloud")
+
+def hello():
+  name = input("what is your name: ")
+  print(f"Hello {name}")
+
+# test function
+hello()
